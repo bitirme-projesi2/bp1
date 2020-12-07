40cac6a8d782594fe77729b49b8b4e9bbaa769a6
Alperen Yurtseven <31616211+alpyurtseven@users.noreply.github.com>
2020-12-07 15:20:01 +0300

    new-script

---
+++ bitirme-projesi1/Assets/Scripts/NewScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewScript : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        rb.AddForce(new Vector3(0,10000,0));
+    }
+}
