c3da9fb8b01b52c7dd7f0acf9dff1931dae0583e
alpereno <alperenonal9922@gmail.com>
2020-12-09 19:00:16 +0300

    movement_script

---
+++ bitirme-projesi1/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed = 4f;
+    Vector3 input;
+    int i = 0, j = 0;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Debug.Log(i++ +" "+input);
+        
+    }
+    private void FixedUpdate()
+    {
+        moveCharacter();
+    }    
+    private void moveCharacter()
+    {
+
+
+        Vector3 inputt = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 direction = input.normalized;
+        Vector3 velocity = speed * direction;
+        Vector3 moveAmount = velocity * Time.deltaTime;
+        rb.MovePosition(transform.position + moveAmount);
+
+        Debug.Log(j++ +" " +inputt+"fffffffffffffffffffffffff");
+    }
+}
