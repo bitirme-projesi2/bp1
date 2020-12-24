380676d580db82c25642298b753d9bc5b3600da8
Alperen Yurtseven <31616211+alpyurtseven@users.noreply.github.com>
2020-12-24 19:00:30 +0300

    camera

--- bitirme-projesi1/Assets/Scripts/cameraController.cs
+++ bitirme-projesi1/Assets/Scripts/cameraController.cs
@@ -8,15 +8,17 @@ public class cameraController : MonoBehaviour
     public float rotateX = 0;
     public float rotateY = 0;
     public Transform Target, Player;
+    bool anm;
+    public Vector3 offset;
     void Start()
     {
-
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        takeAim();
     }
     private void LateUpdate()
     {
@@ -26,9 +28,24 @@ public class cameraController : MonoBehaviour
     {
         rotateX += Input.GetAxis("Mouse X") * sensivity;
         rotateY -= Input.GetAxis("Mouse Y") * sensivity;
-        rotateY = Mathf.Clamp(rotateY, -8, 60);
+        rotateY = Mathf.Clamp(rotateY, -35, 60);
         transform.LookAt(Target);
         Target.rotation = Quaternion.Euler(rotateY, rotateX, 0);
         Player.rotation = Quaternion.Euler(0, rotateX, 0);
+    }
+
+
+    void takeAim()
+    {
+        anm = FindObjectOfType<playerMovement>().isArmed;
+        if (anm && Input.GetKey(KeyCode.Mouse1))
+        {
+            Target.transform.localPosition = offset;
+               
+        }
+        else
+        {
+            Target.transform.localPosition = new Vector3(0,4.04f,0);
+        }
     }
 }
